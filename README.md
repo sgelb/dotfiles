29f4dacf09f9fdbce1b72097dc383f14416210d0
sgelb <stephan.halbritter@haw-hamburg.de>
2015-02-26 13:47:37 +0100

    Initial commit. Added helper

---
+++ README.md
@@ -0,0 +1,5 @@
+### dotfiles
+
+#### link_dotfiles.py
+
+Helper to create symlinks. Edit first!
---
+++ link_dotfiles.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+dotfile_folder = os.path.expanduser('~/code/dotfiles')
+dotfiles = {
+    # source : destination
+    'skel': '~/.vim/skel',
+    'screenrc': '~/.screenrc',
+    'vimrc': '~/.vimrc',
+    'xinitrc': '~/.xinitrc',
+    'zshrc': '~/.zshrc',
+}
+
+ok = '\33[32m::\33[1;0m'
+error = '\33[31m::\33[1;0m'
+
+
+def main():
+    for key, value in dotfiles.items():
+        src = os.path.join(dotfile_folder, key)
+        dst = os.path.expanduser(value)
+        if os.path.exists(src):
+            try:
+                if os.path.islink(dst):
+                    raise FileExistsError
+                os.symlink(src, dst)
+                print('{} Linked {} to {}'.format(ok, src, dst))
+            except FileExistsError:
+                print('{} {} already exists'.format(error, dst))
+        else:
+            print('{} {} does not exist'.format(error, src))
+
+
+if __name__ == '__main__':
+    main()
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
